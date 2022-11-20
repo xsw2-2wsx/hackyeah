ff0045ad8e7ea1be68f23184ea9733bb4776519d
Jan Undrych <jan1undrych@gmail.com>
2022-11-20 01:59:36 +0100

    fix: paged response

--- src/main/kotlin/com/xsw22wsx/hackyeah/utils/PagedResponse.kt
+++ src/main/kotlin/com/xsw22wsx/hackyeah/utils/PagedResponse.kt
@@ -1,8 +1,8 @@
 package com.xsw22wsx.hackyeah.utils
 
 data class PagedResponse<T>(
-    private val numPages: Int,
-    private val entries: Collection<T>
+    val totalEntries: Int,
+    val entries: Collection<T>
 )
 
 fun <T> Collection<T>.paged(numPages: Int) = PagedResponse(numPages, this)
