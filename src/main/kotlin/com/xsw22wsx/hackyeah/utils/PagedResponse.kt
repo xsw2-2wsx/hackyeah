62c3fc5f22b20ac719faf8acfc684a084136c0f9
Jan Undrych <jan1undrych@gmail.com>
2022-11-20 02:15:57 +0100

    refactor: change paged response entires to content

--- src/main/kotlin/com/xsw22wsx/hackyeah/utils/PagedResponse.kt
+++ src/main/kotlin/com/xsw22wsx/hackyeah/utils/PagedResponse.kt
@@ -2,7 +2,7 @@ package com.xsw22wsx.hackyeah.utils
 
 data class PagedResponse<T>(
     val totalEntries: Int,
-    val entries: Collection<T>
+    val content: Collection<T>
 )
 
 fun <T> Collection<T>.paged(numPages: Int) = PagedResponse(numPages, this)
